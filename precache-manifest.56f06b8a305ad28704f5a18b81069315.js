06f404723a245a486837dab29d2a13dfca67e898
pbiti <xenioula123456@gmail.com>
2021-05-21 14:58:15 +0300

    Updates

---
+++ precache-manifest.56f06b8a305ad28704f5a18b81069315.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de359961b25df86d497873733e652591",
+    "url": "/auction-app/index.html"
+  },
+  {
+    "revision": "e870a7dafcbe508fdee9",
+    "url": "/auction-app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "3720d5e4532545b77907",
+    "url": "/auction-app/static/js/2.0019721f.chunk.js"
+  },
+  {
+    "revision": "e870a7dafcbe508fdee9",
+    "url": "/auction-app/static/js/main.4dd76254.chunk.js"
+  },
+  {
+    "revision": "f3bf5b93f7e7136135cd",
+    "url": "/auction-app/static/js/runtime-main.ad8c3a94.js"
+  }
+]);
